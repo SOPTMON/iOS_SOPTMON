9c8839d6db101b4a8c4ccb2bd3d69e8a80e11e92
taehyunL <taylor1233@naver.com>
2022-11-22 17:04:48 +0900

    [ADD] collection view 에 필요한 section 설정

---
+++ TMON-iOS/TMON-iOS/Screens/HomeScene/ListItem.swift
@@ -0,0 +1,16 @@
+//
+//  ListItem.swift
+//  TMON-iOS
+//
+//  Created by 이태현 on 2022/11/22.
+//
+
+import Foundation
+
+struct ListItem {
+    
+    var title: String
+    var image: String
+    var cost: String
+    
+}
---
+++ TMON-iOS/TMON-iOS/Screens/HomeScene/ListSection.swift
@@ -0,0 +1,31 @@
+//
+//  ListSection.swift
+//  TMON-iOS
+//
+//  Created by 이태현 on 2022/11/22.
+//
+
+import Foundation
+
+enum ListSection {
+    
+    case imagePreview([ListItem])
+    case icon([ListItem])
+    case productRecomment([ListItem])
+    case TVONLIVE([ListItem])
+    
+    var items: [ListItem] {
+        switch self {
+        case .imagePreview(let items),
+                .icon(let items),
+                .productRecomment(let items),
+                .TVONLIVE(let items):
+            return items
+        }
+    }
+    
+    var count: Int {
+        return items.count
+    }
+    
+}
