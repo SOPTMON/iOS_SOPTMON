7511291f1657344d45af497a1216fa732b711959
taehyunL <taylor1233@naver.com>
2022-11-23 15:40:53 +0900

    [ADD] TVON LIVE cell

---
+++ TMON-iOS/TMON-iOS/Screens/HomeScene/CollectionViewCells/Cell/TVONLIVECollectionViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  TVONLIVECollectionViewCell.swift
+//  TMON-iOS
+//
+//  Created by 이태현 on 2022/11/22.
+//
+
+import UIKit
+
+class TVONLIVECollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - identifier
+    static let identifier = "TVONLIVECollectionViewCell"
+    
+    //MARK: - UIComponents
+    private let imageView = UIImageView()
+    
+    private let titleLabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.font(.suitRegular, ofSize: 12)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+//        self.backgroundColor = .red
+        addViews()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(_ item: ListItem,_ index: Int) {
+        imageView.image = UIImage(named: item.image)
+        titleLabel.text = item.title
+        titleLabel.textColor = index == 0 ? .mainColor : .grayColor2
+    }
+    
+}
+
+extension TVONLIVECollectionViewCell {
+    
+    private func addViews() {
+        [imageView, titleLabel].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func layout() {
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.height.width.equalTo(70)
+            
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(20)
+        }
+        
+    }
+    
+}
